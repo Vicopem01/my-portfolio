79c6e93cc936158326ec44c8a4382aa326db7a3e
victor <vicopem@gmail.com>
2023-05-16 16:28:02 -0400

    fix: Fixed page crashing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Dock from "@/components/Dock/dock";
+import React from "react";
 import "./globals.css";
 
 export const metadata = {
@@ -5,16 +7,15 @@ export const metadata = {
   description: "Full stack developer ~ Web and blockchain security enthusiat",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className="transition-300 relative m-0 min-h-screen overflow-hidden overflow-x-hidden dark:text-white text-black">
+      <body className="transition-300 relative m-0 min-h-screen dark:text-white text-black">
         {children}
+        <Dock />
       </body>
     </html>
   );
-}
+};
+
+export default Layout;
